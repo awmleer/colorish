85e958c1064f01d2ef1a3604918082a162fe5f58
awmleer <awmleer@sparker.xyz>
2019-04-07 09:06:20 +0800

    add crawler

---
+++ crawler.py
@@ -0,0 +1,20 @@
+import json
+import time
+import requests
+# import colorsys
+
+f = open('data/color.txt', mode='a')
+
+while True:
+    r = requests.post('http://colormind.io/api/', json={
+        'model': 'default'
+    })
+    result = r.json()['result']
+    print(result)
+    # converted = []
+    # for c in result:
+    #     converted.append(colorsys.rgb_to_hsv(c[0], c[1], c[2]))
+    # print(converted)
+    f.write(json.dumps(result) + '\n')
+    time.sleep(1)
+
