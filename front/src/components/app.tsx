e18668b69f8979efd9ed34ba586fe95ae633141e
awmleer <awmleer@sparker.xyz>
2019-04-21 19:27:11 +0800

    color type

--- front/src/components/app.tsx
+++ front/src/components/app.tsx
@@ -1,16 +1,21 @@
-import {FC} from 'react'
+import {FC, useState} from 'react'
 import * as React from 'react'
 import styled from 'styled-components'
 import {NavBar} from './nav-bar'
+import {Color} from '../classes/color'
 
 const Header = styled.div`
   padding: 12px;
 `
 
 export const App: FC = function() {
+  const [schema, setSchema] = useState<Color[]>(null)
   return (
     <>
       <NavBar/>
+      <button className="button is-success">
+        Generate
+      </button>
     </>
   )
 }
