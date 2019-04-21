d49442e2a672568f888ee0675f9ed28ad4bddf62
awmleer <awmleer@sparker.xyz>
2019-04-21 19:08:05 +0800

    nav bar

--- front/src/components/app.tsx
+++ front/src/components/app.tsx
@@ -1,10 +1,16 @@
 import {FC} from 'react'
 import * as React from 'react'
+import styled from 'styled-components'
+import {NavBar} from './nav-bar'
+
+const Header = styled.div`
+  padding: 12px;
+`
 
 export const App: FC = function() {
   return (
-    <div>
-      aaa
-    </div>
+    <>
+      <NavBar/>
+    </>
   )
 }
---
+++ front/src/components/nav-bar.tsx
@@ -0,0 +1,26 @@
+import {memo} from 'react'
+import * as React from 'react'
+import styled from 'styled-components'
+
+
+const Root = styled.nav`
+  border-bottom: solid 2px #f0f0f0;
+`
+
+export const NavBar = memo(function NavBar(){
+  return (
+    <Root className="navbar" role="navigation">
+      <div className="navbar-brand">
+        <h1 className="navbar-item title is-4">Colorish</h1>
+      </div>
+      
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">
+            Test
+          </a>
+        </div>
+      </div>
+    </Root>
+  )
+})
