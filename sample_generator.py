b33d99062bc0110a41e513b42a20857dfde44f5e
awmleer <awmleer@sparker.xyz>
2019-05-14 16:46:22 +0800

    trained more models

--- sample_generator.py
+++ sample_generator.py
@@ -1,12 +1,8 @@
-import colorsys
-import json
-import random
 import sys
-
 import torch
 
-from networks import RNN, Discriminator, Generator
-from utils import tensorToRGB, color_list_to_tensor, tensor_to_color_list
+from networks import Discriminator, Generator
+from utils import tensor_to_color_list
 
 discriminator = Discriminator()
 discriminator.load_state_dict(torch.load('data/discriminator-state-dict'))
@@ -21,8 +17,8 @@ def sample():
         print('<div class="color-block" style="background-color: rgb(%d, %d, %d)"></div>' % (color[0], color[1], color[2]))
     return colors
 
-# for i in range(int(sys.argv[1])):
-#     print('<div class="schema">')
-#     sample()
-#     print('</div>')
-#     print('')
+for i in range(int(sys.argv[1])):
+    print('<div class="schema">')
+    sample()
+    print('</div>')
+    print('')
