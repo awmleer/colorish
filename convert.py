0c386d185b09038994c92b1d7af59ad8031b22c9
awmleer <awmleer@sparker.xyz>
2019-05-18 11:20:44 +0800

    convert sample data from csv

---
+++ convert.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+csv_file = open('data/kuler.csv')
+color_file = open('data/kuler.txt', mode='a')
+
+reader = csv.reader(csv_file)
+count = 0
+for row in reader:
+    count += 1
+    print(count)
+    data = json.loads(json.loads(row[1]))
+    colors = []
+    for swatch in data['swatches']:
+        h = swatch['hex']
+        colors.append(list(int(h[i:i + 2], 16) for i in (0, 2, 4)))
+    color_file.write(json.dumps(colors) + '\n')
--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Sample(models.Model):
+    colors = models.CharField(max_length=200)
+
+
