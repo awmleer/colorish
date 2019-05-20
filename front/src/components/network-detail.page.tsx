632a217bd84eebf018ee1faab1e8c83f1693e7d9
awmleer <awmleer@sparker.xyz>
2019-05-20 09:54:47 +0800

    training log chart

--- front/src/components/network-detail.page.tsx
+++ front/src/components/network-detail.page.tsx
@@ -1,9 +1,10 @@
 import {RouteComponentProps, withRouter} from 'react-router'
-import {memo, useEffect, useState} from 'react'
+import {memo, useEffect, useRef, useState} from 'react'
 import * as React from 'react'
 import {Network} from '../classes/network'
 import {apiService} from '../services/api.service'
 import {PopularSchemas} from './popular-schemas'
+import Chart from 'chart.js';
 
 interface Props extends RouteComponentProps<{
   id: string
@@ -11,7 +12,7 @@ interface Props extends RouteComponentProps<{
 
 export const NetworkDetailPage = withRouter<Props>(memo(function NetworkDetailPage(props) {
   const [network, setNetwork] = useState<Network>(null)
-  
+  const chartCanvasRef = useRef<HTMLCanvasElement>()
   
   async function fetchData() {
     setNetwork(await apiService.get(`network/${props.match.params.id}`))
@@ -19,7 +20,44 @@ export const NetworkDetailPage = withRouter<Props>(memo(function NetworkDetailPa
   
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [props.match.params.id])
+  
+  useEffect(() => {
+    if (!network) return
+    const labels = []
+    const dataD = []
+    const dataG = []
+    let count = 0
+    for (const round of network.log.rounds) {
+      if (count%2 === 0) labels.push((count/2 + 1).toString())
+      count++
+      if (round.mode === 'D') {
+        dataD.push(round.loss)
+      } else {
+        dataG.push(round.loss)
+      }
+    }
+    const ctx = chartCanvasRef.current.getContext('2d')
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Discriminator',
+          data: dataD,
+          borderColor: 'rgb(255,135,99)',
+          backgroundColor: 'rgb(255,193,178)',
+          fill: false,
+        }, {
+          label: 'Generator',
+          data: dataG,
+          borderColor: 'rgb(115,136,255)',
+          backgroundColor: 'rgb(184,190,255)',
+          fill: false,
+        }]
+      }
+    })
+  }, [network])
   
   return network && (
     <>
@@ -32,6 +70,7 @@ export const NetworkDetailPage = withRouter<Props>(memo(function NetworkDetailPa
       <section className='section'>
         <div className='container'>
           <h3 className='title is-4'>Training Log</h3>
+          <canvas ref={chartCanvasRef} height='200' width='400'/>
         </div>
       </section>
       <section className='section'>
